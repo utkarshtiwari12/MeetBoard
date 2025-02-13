21dde7ae18f375411900068c1571ac781ed54817
Utkarsh Tiwari <utkarshthor12@gmail.com>
2025-02-13 10:49:39 +0530

    configured clerk and covex

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
-import { Button } from "@/components/ui/button";
-
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <Button>Click Me</Button>
+    <div className="flex flex-col gap-y-4">
+      <div>
+      This is a screen for authenticated users only...
+      </div>
+
+      <div>
+        <UserButton />
+      </div>
+    </div>
   );
 }
