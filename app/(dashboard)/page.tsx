e4f94e593540b02d2d219847bf3256658f7b7fff
Utkarsh Tiwari <utkarshthor12@gmail.com>
2025-02-21 19:06:40 +0530

    created pages for empty states

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,11 +1,24 @@
+"use client";
 
+import { useSearchParams } from "next/navigation";
+import { useOrganization } from "@clerk/nextjs";
+import { EmptyOrg } from "./_components/empty-org";
+import { BoardList } from "./_components/board-list";
 
 export default function DashBoardPage() {
+  const searchParams = useSearchParams();  
+  const search = searchParams.get("search") || "";  
+  const favorites = searchParams.get("favorites") || "";  
+  const { organization } = useOrganization();
+
   return (
-    <div className="flex flex-col gap-y-4">
-      <div>
-      DashBoard Root Page.
-      </div>
+    <div className="flex-1 h-[calc(100%-80px)] p-6">
+      {!organization ? <EmptyOrg /> : (
+        <BoardList
+          orgId={organization.id}
+          query={{ search, favorites }}  
+        />
+      )}
     </div>
   );
 }
