3f310c03ca78432b9237907d21e0a2a4262cb36e
Utkarsh Tiwari <utkarshthor12@gmail.com>
2025-02-22 11:26:44 +0530

    created board db schema

--- app/(dashboard)/_components/empty-boards.tsx
+++ app/(dashboard)/_components/empty-boards.tsx
@@ -1,7 +1,34 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
 import Image from "next/image";
+import { useOrganization } from "@clerk/nextjs";
+import { toast } from "sonner";
 
 export const EmptyBoards = () => {
+
+    const { organization } = useOrganization();
+
+    const create = useMutation(api.board.create);
+
+    const onClick = () => {
+
+        if (!organization) return;
+
+        create({
+            orgId: organization?.id,
+            title: "Untitled"
+        })
+            .then((id) => {
+                toast.success("Board Created");
+
+                // TODO : redirect to board/{id}
+            })
+            .catch(() => toast.error("Failed to create board"));
+    }
+
     return (
         <div className="h-full flex flex-col items-center justify-center">
             <Image
@@ -17,7 +44,7 @@ export const EmptyBoards = () => {
                 Start by creating a board for your organization.
             </p>
             <div className="mt-6">
-                <Button size='lg'>Create Board</Button>
+                <Button onClick={onClick} size='lg'>Create Board</Button>
             </div>
         </div>
     )
