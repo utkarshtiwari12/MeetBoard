3b9b279206264c3a5e85dabad1dcd6ef72678556
Utkarsh Tiwari <utkarshthor12@gmail.com>
2025-03-09 10:50:24 +0530

    participants section

--- app/board/[boardId]/_components/participants.tsx
+++ app/board/[boardId]/_components/participants.tsx
@@ -1,7 +1,50 @@
+"use client";
+
+import { useOthers, useSelf } from "@/liveblocks.config";
+
+import { UserAvatar } from "./user-avatar";
+import { connectionIdToColor } from "@/lib/utils";
+
+const MAX_SHOWN_USERS = 2; //other than the host itself
+
 export const Participants = () => {
+
+    const users = useOthers();
+    const currentUser = useSelf();
+    const hasMoreUsers = users.length > MAX_SHOWN_USERS;
+
     return (
         <div className="top-2 absolute h-12 right-2 bg-white rounded-md p-3 flex items-center shadow-md">
-            List of Participants.
+            <div className="flex gap-x-2">
+                {users.slice(0, MAX_SHOWN_USERS)
+                    .map(({ connectionId, info }) => {return (
+                        <UserAvatar
+                            borderColor={connectionIdToColor(connectionId)}
+                            key={connectionId}
+                            src={info?.picture}
+                            name={info?.name}
+                            fallback={info?.name?.[0] || "A"}
+                        />
+                    )
+                    })}
+                
+                {currentUser && (
+                    <UserAvatar
+                    borderColor={connectionIdToColor(currentUser.connectionId)}
+                        
+                        src={currentUser.info?.picture}
+                        name={`${currentUser.info?.name} (You)`}
+                        fallback={currentUser.info?.name?.[0]}
+                    />
+                )}
+
+                {hasMoreUsers && (
+                    <UserAvatar
+                        name={`${users.length - MAX_SHOWN_USERS} more`}
+                        fallback={`+${users.length - MAX_SHOWN_USERS}`}
+                    />
+                )}
+            </div>
         </div>
     )
 }
