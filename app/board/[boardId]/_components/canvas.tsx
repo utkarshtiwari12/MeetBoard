1d20e63addc8f6840bce50306dc54cdf5720b461
Utkarsh Tiwari <utkarshthor12@gmail.com>
2025-03-18 21:01:30 +0530

    created functionality to select multiple layers

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -9,7 +9,7 @@ import React, { useCallback, useMemo, useState } from "react";
 import { Camera, CanvasMode, CanvasState, Color, LayerType, Point, Side, XYWH } from "@/types/canvas";
 import { useCanRedo, useCanUndo, useHistory, useMutation, useOthersMapped, useStorage } from "@/liveblocks.config";
 import { CursorsPresence } from "./cursors-presence";
-import { connectionIdToColor, pointerEventToCanvasPoint, resizeBounds } from "@/lib/utils";
+import { connectionIdToColor, findIntersectinglayersWithRectangle, pointerEventToCanvasPoint, resizeBounds } from "@/lib/utils";
 import { nanoid } from "nanoid";
 import { LiveObject } from "@liveblocks/client";
 import { LayerPreview } from "./layer-preview";
@@ -111,6 +111,37 @@ const Canavs = ({
     }
   }, [])
 
+  const updateSelectionNet = useMutation((
+    { storage, setMyPresence },
+    current: Point,
+    origin: Point
+  ) => {
+    const layers = storage.get("layers").toImmutable();
+
+    setCanvasState({
+      mode: CanvasMode.SelectionNet,
+      origin, 
+      current
+    })
+
+    const ids = findIntersectinglayersWithRectangle(layerIds, layers, origin, current);
+
+    setMyPresence({ selection: ids });
+  }, [layerIds])
+
+  const startMultiSelection = useCallback((
+    current: Point,
+    origin: Point
+  ) => { 
+    if (Math.abs(current.x - origin.x) + Math.abs(current.y - origin.y) > 5) {
+      setCanvasState({
+        mode: CanvasMode.SelectionNet,
+        origin,
+        current
+      })
+    }
+  }, []);
+
   const resizeSelectedLayer = useMutation((
     { storage, self },
     point: Point
@@ -157,7 +188,13 @@ const Canavs = ({
     e.preventDefault();
     const current = pointerEventToCanvasPoint(e, camera);
 
-    if (canvasState.mode === CanvasMode.Translating) {
+    if (canvasState.mode === CanvasMode.Pressing) {
+      startMultiSelection(current, canvasState.origin);
+    }
+    else if (canvasState.mode === CanvasMode.SelectionNet) {
+      updateSelectionNet(current, canvasState.origin);
+    }
+    else if (canvasState.mode === CanvasMode.Translating) {
       translateSelectedLayers(current);
     }
     else if (canvasState.mode === CanvasMode.Resizing) {
@@ -287,6 +324,15 @@ const Canavs = ({
           <SelectionBox
             onResizeHandlePointerDown={onResizeHandlePointerDown}
           />
+          {canvasState.mode === CanvasMode.SelectionNet && canvasState.current != null && (
+            <rect
+              className="fill-blue-500/5 stroke-blue-500 stroke-1"
+              x={Math.min(canvasState.origin.x, canvasState.current.x)}
+              y={Math.min(canvasState.origin.y, canvasState.current.y)}
+              width={Math.abs(canvasState.origin.x - canvasState.current.x)}
+              height={Math.abs(canvasState.origin.y - canvasState.current.y)}
+            />
+          )}
           <CursorsPresence />
         </g>
       </svg>
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,4 @@
-import { Camera, Color, Point, Side, XYWH } from "@/types/canvas";
+import { Camera, Color, Layer, Point, Side, XYWH } from "@/types/canvas";
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
@@ -65,4 +65,42 @@ export function resizeBounds(
   }
 
   return result;
+};
+
+
+export function findIntersectinglayersWithRectangle(
+  layerIds: readonly string[],
+  layers: ReadonlyMap<string, Layer>,
+  a: Point,
+  b: Point
+) {
+  const rect = {
+    x: Math.min(a.x, b.x),
+    y: Math.min(a.y, b.y),
+    width: Math.abs(a.x - b.x),
+    height: Math.abs(a.y - b.y),
+  };
+
+  const ids = [];
+
+  for (const layerId of layerIds) {
+    const layer = layers.get(layerId);
+
+    if (layer == null) {
+      continue;
+    }
+
+    const { x, y, width, height } = layer;
+
+    if (
+      rect.x + rect.width > x &&
+      rect.x < x + width &&
+      rect.y + rect.height > y &&
+      rect.y < y + height
+    ) {
+      ids.push(layerId);
+    }
+  }
+
+  return ids;
 }
