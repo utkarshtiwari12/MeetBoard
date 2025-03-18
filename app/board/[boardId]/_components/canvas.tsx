6005880d5eaa5dcec8bfb9c9820f05edece71897
Utkarsh Tiwari <utkarshthor12@gmail.com>
2025-03-18 09:15:08 +0530

    implemented color change and deletion

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -14,6 +14,7 @@ import { nanoid } from "nanoid";
 import { LiveObject } from "@liveblocks/client";
 import { LayerPreview } from "./layer-preview";
 import { SelectionBox } from "./selection-box";
+import { SelectionTools } from "./selection-tools";
 
 const MAX_LAYERS = 100;
 
@@ -256,6 +257,11 @@ const Canavs = ({
         canRedo={canRedo}
         undo={history.undo}
         redo={history.redo}
+      />
+
+      <SelectionTools
+        camera={camera}
+        setLastUsedColor={setLastUsedColor}
       />
 
       <svg className="h-[100vh] w-[100vw]"
